ed4d50011af182e5997dc789c3f2cbdee5f46714
darkwizard242 <am900820@gmail.com>
2020-12-05 22:31:27 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,13 +6,26 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+PACKAGE = "subfinder"
+PACKAGE_BINARY = "/usr/local/bin/subfinder"
+
+
 def test_subfinder_binary_exists(host):
-    assert host.file('/usr/local/bin/subfinder').exists
+    """
+    Tests if subfinder binary file exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
 def test_subfinder_binary_file(host):
-    assert host.file('/usr/local/bin/subfinder').is_file
+    """
+    Tests if subfinder binary is a file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
 def test_subfinder_binary_which(host):
-    assert host.check_output('which subfinder') == '/usr/local/bin/subfinder'
+    """
+    Tests the output to confirm subfinder's binary location.
+    """
+    assert host.check_output('which subfinder') == PACKAGE_BINARY
